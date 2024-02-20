b6c37a321155910ea264e15fb16e82bee0f7263c
MikePik18 <michaelpikounis18@gmail.com>
2024-02-19 23:16:55 -0500

    Added multiply method

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -9,10 +9,10 @@ public class SimpleCalculator {
 		return a - b;
 	}
 	
-//	public int multiply(int a, int b) {
-//		return a * b;
-//	}
-//	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
 //	public int divide(int a, int b) {
 //		return a / b;
 //	}
